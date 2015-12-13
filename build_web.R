274bbec0c2e2d30f75001a108ea04f02c1c4e26b
mcomas <marc@macmini.local>
2015-12-13 21:05:58 +0100

    Arxiu per crear la web

---
+++ build_web.R
@@ -0,0 +1,9 @@
+library(rmarkdown)
+
+build = function(src) rmarkdown::render(input=sprintf('%s.Rmd', src),output_dir='www')
+
+build('index')
+build('R_Rcmdr')
+build('desc_stat')
+build('inf_stat')
+build('models')
